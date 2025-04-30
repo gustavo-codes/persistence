4afc0e1a30e478231a0f80774c21f0d23eececf8
Mário Aragão <116241488+sincendz@users.noreply.github.com>
2025-04-30 01:13:43 +0000

    codigo completo

--- FastAPI and XML/main.py
+++ FastAPI and XML/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
-import json
 from pathlib import Path
 from xml.etree import ElementTree as ET
 
@@ -12,12 +11,23 @@ class Livro(BaseModel):
     ano:int
     genero:str
 
+pasta = Path('db')
+pasta.mkdir(exist_ok=True)
+
+LIVROS_XML = pasta / 'livros.xml'
+
+if not LIVROS_XML.exists():
+    root = ET.Element("livros")
+    tree = ET.ElementTree(root)
+    tree.write(LIVROS_XML)
+
+
 app = FastAPI()
 
 #Lê o XML e retorna uma lista de Livros
 def readXml():
     livros = []
-    tree = ET.parse('livros.xml')
+    tree = ET.parse(LIVROS_XML)
     root = tree.getroot()
     
     for elem in root.findall('livro'):
@@ -44,8 +54,28 @@ def writeXml(livros):
         ET.SubElement(livroElement,'ano').text = str(livro.ano)
         ET.SubElement(livroElement,'genero').text = livro.genero
 
-        tree = ET.ElementTree(root)
-        tree.write('livros.xml')
+    tree = ET.ElementTree(root)
+    tree.write(LIVROS_XML)
+
+
+def update_livro(livro_id: int, livro:Livro):
+    tree = ET.parse(LIVROS_XML)
+    root = tree.getroot()
+    
+    for l in root.findall("livro"):
+        print(int(l.find("id").text))
+        if int(l.find("id").text) == livro_id:
+            if livro.id != livro_id:
+                livro.id = livro_id
+            l.find("titulo").text = livro.titulo
+            l.find("autor").text = livro.autor
+            l.find("ano").text = str(livro.ano)
+            l.find("genero").text = livro.genero
+            tree.write(LIVROS_XML)
+            return livro
+    raise HTTPException(status_code=404,detail="ID não foi encontrado")
+    
+
 
 @app.get('/livros/',response_model=List[Livro])
 def getLivros():
@@ -66,9 +96,22 @@ def addLivro(livro:Livro):
 def getLivro(livroId:int):
     livros = readXml()
     
-    if not any(l.id == livroId for l in livros):
-       raise HTTPException(status_code=400,detail='Livro não encontrado')
-
     for livro in livros:
         if livro.id == livroId:
             return livro
+    raise HTTPException(status_code=400,detail='Livro não encontrado')
+
+@app.put("/livros/{livro_id}", response_model=Livro)
+def update_book_by_id(livro_id:int,book:Livro):
+    return update_livro(livro_id,book)
+    
+    
+@app.delete("/livros/{livro_id}")
+def delete_book_by_id(livro_id:int):
+    books = readXml()
+    if not any(l.id == livro_id for l in books):
+        raise HTTPException(status_code=404,detail="ID não existe")
+    new_books = [book for book in books if book.id != livro_id]
+    writeXml(new_books)
+    return {"msg":f"Livro de id {livro_id} foi apagado."}
+    
